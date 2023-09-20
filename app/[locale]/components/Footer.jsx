a0d6de7af2ccd26dec185773243ab8b5c10ec713
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-09-20 21:09:23 +0200

    new footer added

--- app/[locale]/components/Footer.jsx
+++ app/[locale]/components/Footer.jsx
@@ -4,6 +4,7 @@ import { useTranslations } from "next-intl";
 
 const Footer = () => {
   const t = useTranslations("Locale");
+  const p = useTranslations("Footer");
   return (
     <div className="h-32 bg-black flex items-center  md:justify-between">
       <div className="mx-auto flex flex-col md:flex-row gap-x-96  gap-y-4 md:text-center justify-between">
@@ -16,26 +17,26 @@ const Footer = () => {
 
         <div className="list-none text-semibold text-sm md:text-md flex  gap-x-4 md:gap-x-16 roboto700 text-primaryYellow">
           <Link href={`${t("locale")}`}>
-            <li className="relative group">
-              HOME
-              <div className="w-0 bg-primaryYellow h-under  opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300" />
+            <li className="relative group text-xs md:text-sm ">
+              {p("home")}
+              <div className="w-0 bg-primaryYellow h-under   opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300" />
             </li>
           </Link>
 
           <Link href={`${t("locale")}/terms`}>
-            <li className="relative group">
-              TERMS
+            <li className="relative group text-xs md:text-sm">
+              {p("terms")}
               <div className="w-0 bg-primaryYellow h-under  opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300" />
             </li>
           </Link>
           <Link href={`${t("locale")}/about`}>
-            <li className="relative group">
-              ABOUT
+            <li className="relative group text-xs md:text-sm">
+              {p("about")}
               <div className="w-0 bg-primaryYellow h-under  opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300" />
             </li>
           </Link>
           <Link href={`${t("locale")}/faq`}>
-            <li className="relative group">
+            <li className="relative group text-xs md:text-sm">
               FAQ
               <div className="w-0 bg-primaryYellow h-under  opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-300" />
             </li>
